918dfd28224b548bfd7f52b61342f947cf33fe1e
Edmundo Carmona <carmona.edmundo@gmail.com>
2015-11-21 23:15:25 -0600

    adding the run_analysis.R final version

--- run_analysis.R
+++ run_analysis.R
@@ -16,6 +16,6 @@ tidy_data2<-select(tidy_data,grep('V1|mean|std',names(tidy_data)))
 activity_labels<-read.table("activity_labels.txt",stringsAsFactors = F)
 tidy_data3<-merge(tidy_data2,activity_labels,by.x = "activity.V1",by.y = "V1")
 tidy_data4<-rename(tidy_data3,ActivityName=V2,ActivityCode=activity.V1,Subject=subject.V1)
-?summarise_each
- 
+
 tidy_data5<-tidy_data4 %>% group_by(ActivityName,ActivityCode,Subject)%>% summarize_each(funs(mean))
+write.table(tidy_data5,file = "tidy_data.txt",row.name=FALSE)
